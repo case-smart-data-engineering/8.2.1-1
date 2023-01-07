a2130759cd2bb9b3eb7f68123cf0698f999e5148
Ricardo M. Shi <38983771+KaiserLord@users.noreply.github.com>
2023-01-07 15:19:45 +0800

    上传代码文件

---
+++ 1_算法示例/clue_process.py
@@ -0,0 +1,83 @@
+import json
+import sys
+import os
+# sys.path.append('D:\GitHub\BERT-BiLSTM-CRF-NER-pytorch\\1_算法示例')
+
+def _read_json(input_file, mode="train"):
+    lines = []
+    with open(input_file,'r', encoding='utf-8') as f:
+        for line in f:
+            line = json.loads(line.strip())
+            text = line['text']
+            label_entities = line.get('label',None)
+            words = list(text)
+            labels = ['O'] * len(words)
+            if label_entities is not None:
+                for key,value in label_entities.items():
+                    for sub_name,sub_index in value.items():
+                        for start_index,end_index in sub_index:
+                            assert  ''.join(words[start_index:end_index+1]) == sub_name
+                            if start_index == end_index:
+                                labels[start_index] = 'B-'+key
+                            else:
+                                labels[start_index] = 'B-'+key
+                                labels[start_index+1:end_index+1] = ['I-'+key]*(len(sub_name)-1)
+            lines.append({"words": words, "labels": labels})
+    
+
+    with open(f"d:\\GitHub\\BERT-BiLSTM-CRF-NER-pytorch\\1_算法示例\data\clue\{mode}.txt", "w", encoding='utf-8') as f:
+        for line in lines:
+            for w, l in zip(line["words"], line["labels"]):
+                f.write(f"{w}\t{l}\n")
+            f.write("\n")
+
+def get_entity_bio(seq):
+    """Gets entities from sequence.
+    note: BIO
+    Args:
+        seq (list): sequence of labels.
+    Returns:
+        list: list of (chunk_type, chunk_start, chunk_end).
+    Example:
+        seq = ['B-PER', 'I-PER', 'O', 'B-LOC']
+        get_entity_bio(seq)
+        #output
+        [['PER', 0,1], ['LOC', 3, 3]]
+    """
+    chunks = []
+    chunk = [-1, -1, -1]
+    for indx, tag in enumerate(seq):
+        # if not isinstance(tag, str):
+        #     tag = id2label[tag]
+        if tag.startswith("B-"):
+            if chunk[2] != -1:
+                chunks.append(chunk)
+            chunk = [-1, -1, -1]
+            chunk[1] = indx
+            chunk[0] = tag.split('-')[1]
+            chunk[2] = indx
+            if indx == len(seq) - 1:
+                chunks.append(chunk)
+        elif tag.startswith('I-') and chunk[1] != -1:
+            _type = tag.split('-')[1]
+            if _type == chunk[0]:
+                chunk[2] = indx
+
+            if indx == len(seq) - 1:
+                chunks.append(chunk)
+        else:
+            if chunk[2] != -1:
+                chunks.append(chunk)
+            chunk = [-1, -1, -1]
+
+    return chunks
+
+if __name__ == "__main__":
+    file_path = 'd:\\GitHub\\BERT-BiLSTM-CRF-NER-pytorch\\1_算法示例\data\clue'
+    train_file = file_path+'\\train.json'
+    eval_file = file_path+'\eval.json'
+    test_file = file_path+'\\test.json'
+
+    _read_json(train_file, "train")
+    _read_json(eval_file, "eval")
+    _read_json(test_file, "test")
---
+++ 1_算法示例/readme.md
@@ -0,0 +1,83 @@
+# BERT-BiLSTM-CRF模型
+
+### 文件清单
+- data：数据集
+- model：模型参数、保存训练后的模型、模型输入所需的文件
+- pretrained_bert_model: bert模型存放路径
+- clue_process.py：数据处理文件
+- conlleval.py：模型评估代码
+- models.py：BERT+BiLstm+CRF模型
+- ner.py：运行入口
+- utils.py：工具文件
+
+### 运行的环境
+```
+python == 3.7.4
+pytorch == 1.3.1 
+pytorch-crf == 0.7.2  
+pytorch-transformers == 1.2.0       
+
+### 数据处理
+
+运行`clue_process.py`文件对数据集进行处理
+把数据从json文件处理成BIO格式，保存在txt同名文件
+
+```
+彭	B-name
+小	I-name
+军	I-name
+认	O
+为	O
+，	O
+国	O
+内	O
+银	O
+行	O
+现	O
+在	O
+走	O
+的	O
+是	O
+台	B-address
+湾	I-address
+
+温	B-name
+格	I-name
+的	O
+球	O
+队	O
+终	O
+于	O
+```
+
+        
+```
+### 使用方法
+```
+BERT_BASE_DIR=bert-base-chinese
+DATA_DIR=/data/clue/
+OUTPUT_DIR=./model/clue_bilstm
+export CUDA_VISIBLE_DEVICES=0
+
+python ner.py \
+    --model_name_or_path ${BERT_BASE_DIR} \
+    --do_train True \
+    --do_eval True \
+    --do_test True \
+    --max_seq_length 256 \
+    --train_file ${DATA_DIR}/train.txt \
+    --eval_file ${DATA_DIR}/dev.txt \
+    --test_file ${DATA_DIR}/test.txt \
+    --train_batch_size 32 \
+    --eval_batch_size 32 \
+    --num_train_epochs 10 \
+    --do_lower_case \
+    --logging_steps 200 \
+    --need_birnn True \
+    --rnn_dim 256 \
+    --clean True \
+    --output_dir $OUTPUT_DIR
+```
+
+### 在中文CLUENER2020的eval集上的结果
+https://github.com/CLUEbenchmark/CLUENER2020
